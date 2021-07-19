2aae82c3fecbbfedcf4f6717e2082fb63b922372
sadiq-b <babalolasadiq@yahoo.com>
2021-07-19 22:34:39 +0100

    ceate Linegraph component

---
+++ src/Components/LineGraph.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+
+const LineGraph = () => {
+    return (
+        <div>
+            <Line data={{
+                labels: ['1', '2', '3', '4', '5', '6'],
+                datasets: [
+                    {
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        fill: false,
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgba(255, 99, 132, 0.2)',
+                    },
+                ],
+            }} />
+        </div>
+    )
+}
+
+export default LineGraph
