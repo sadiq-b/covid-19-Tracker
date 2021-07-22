ca8f07fa59315fb90c681524ce9e14c64ec437c6
sadiq-b <babalolasadiq@yahoo.com>
2021-07-22 01:15:25 +0100

    fetching 7 days covid report

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 create project folder npx create-react-app projectname
 install dependencies npm --save axios react-chartjs-2 chart.js react-number-format
 create LineGraph component
-create Graph.js component
+create Card.js component
+create CovidSummary File
+create axios.js File
--- src/App.js
+++ src/App.js
@@ -1,15 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LineGraph from './Components/LineGraph';
 import './App.css';
-import CovidSummary from './Components/CovidSummary'
+import CovidSummary from './Components/CovidSummary';
+import axios from './axios';
 const App = () => {
+
+    const [ConfirmedCases, setConfirmedCases] = useState(0);
+    const [TotalRecovered, setTotalRecovered] = useState(0);
+    const [TotalDeaths, setTotalDeaths] = useState(0);
+    const [Loading, setLoading] = useState(false);
+    const [CovidDetails, setCovidDetails] = useState({});
+    const [Days,setDays]=useState(7);
+    const [Country,setCountry]=useState('')
+
+    //componentdidmount
+    useEffect(() => {
+        setLoading(true)
+        axios.get(`./summary`)
+            .then(res => {
+                setLoading(false)
+                if (res.status === 200) {
+                    setConfirmedCases(res.data.Global.TotalConfirmed)
+                    setTotalRecovered(res.data.Global.TotalRecovered)
+                    setTotalDeaths(res.data.Global.TotalDeaths)
+                    setCovidDetails(res.data)
+                    console.log(res)
+                }
+
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+ 
+
+    const formatDate=(date)=>{
+        const d =new Date(date);
+       // 2021-07-21
+        const year=d.getFullYear();
+        const month=`0${d.getMonth()+1}`.slice(-2);
+        const day=d.getDate();
+
+        return `${year}-${month}-${day}`;
+    }
+    const countryChangeHandler=(e)=>{
+        setCountry(e.target.value);
+        const d=new Date();
+        const to=formatDate(d);
+        const from= formatDate(d.setDate(d.getDate()-7));
+        //console.log(from,to)
+        coronaDateRange(e.target.value,from,to);
+
+    }
+
+    const dayChangeHandler=(e)=>{
+        setDays(e.target.value)
+    }
+    
+    const coronaDateRange=(countrySlug,from,to)=>{
+        axios.get(`/country/${countrySlug}/status/confirmed?from=${from}T00:00:00Z&to=${to}T00:00:00Z`)
+        .then(res=>{
+            console.log(res);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+
+    }
+    if (Loading) {
+        return <p> Fething data from server.... </p>
+    }
     return (
         <div className='App'>
             <CovidSummary
-            ConfirmedCases={0}
-            TotalRecovered ={0}
-            TotalDeaths={0}
-            Country={'Nigeria'}/>
+                ConfirmedCases={ConfirmedCases}
+                TotalRecovered={TotalRecovered}
+                TotalDeaths={TotalDeaths}
+                Country={'Nigeria'} />
+            <div>
+                <select value={Country} onChange={countryChangeHandler}>
+                    {CovidDetails.Countries && CovidDetails.Countries.map(country => <option key={country.Slug} value={country.Slug}>{country.Country}</option>)}
+                </select > 
+                <select value={Days} onChange={dayChangeHandler}>
+                    <option value="7">Last 7 days</option>
+                    <option value="30">Last 30 days</option>
+                    <option value="90">Last 90 days</option>
+                </select>
+
+            </div>
             <LineGraph />
         </div>
     )
